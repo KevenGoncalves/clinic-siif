a7b9d7956310681d0986a82a97b288d144d84e29
Keven <80492657+KevenGoncalves@users.noreply.github.com>
2022-11-15 09:11:53 +0200

    correcting head import

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { Head } from "next/document";
+import Head from "next/head";
 import Cta from "../components/home/cta";
 import Features from "../components/home/features";
 import Footer from "../components/home/footer";
