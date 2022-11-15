e9a4586fe2603efe756002411226510285ed3212
Keven <80492657+KevenGoncalves@users.noreply.github.com>
2022-11-15 04:10:34 +0200

    database added to planetscale fixed 3

--- src/components/agendas/agenda-passada.tsx
+++ src/components/agendas/agenda-passada.tsx
@@ -28,7 +28,7 @@ const Card = ({
 			<div>
 				{getPaciente.data?.user.firstName} {getPaciente.data?.user.lastName} - {content.date}
 			</div>
-			{content.consultaState === "REJEITADA" ? (
+			{(content.consultaState as any) === "REJEITADA" ? (
 				<div className="text-red-500 p-1" title="Rejeitada">
 					<Cross />
 				</div>
